224c14011ff5bf2d169e7036d5b36efe13c38e1c
Adam SOudani <asoudani@email.com>
2020-03-18 14:12:32 -0700

    create link to GIPHY API with ajax call

--- gif.js
+++ gif.js
@@ -1,14 +1,25 @@
 //Sports Icons
 var topics = ['Muhammed Ali', 'Kobe Bryant', 'Lebron James', 'Michael Jordan', 'Tom Brady', 'Peyton Manning', 'Joe Montana', 'Floyd Mayweather', 'Jerry Rice', 'Tiger Woods', 'Arnold Palmer', 'Phil Mickelson', 'Roger Federer', 'Serena Williams', 'Maria Sharapova', 'Danica Patrick', 'Mike Tyson', 'Connor Mcgregor', 'Jon Jones', 'Emmit Smith', 'Magic Johnson', 'Larry Bird', 'Lisa Leslie', 'Lionel Messi', 'Cristiano Ronaldo'];
 
-var search = '&q=SportsIcons&limit=100';
-
 function displaySportsInfo(){
+    
     var sportData = $(this).attr('data-name');
-    var queryURL = 'https://api.giphy.com/v1/gifs/search' + sportData + 'api_key=McsnolR8TBWZheBnkbVE8Cczf83ggjI1';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search&q=' + sportData + 'api_key=McsnolR8TBWZheBnkbVE8Cczf83ggjI1&limit=10&rating';
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+
+        var sportDiv = $("<div class='sportsmen'>");
+
+        var rating = response.rating;
+
+        var displayRating = $("<p>").text("Rating: " + rating);
+
+        sportDiv.append(displayRating);
+    })
 }
-
-
 
 
 function renderButtons(){
@@ -17,7 +28,7 @@ function renderButtons(){
 
     for (var i = 0; i < topics.length; i++){
         var btn = $('<button>');
-        btn.addClass('sportsmen');
+        btn.addClass('sportsmen-btn');
         btn.attr('data-name', topics[i]);
         btn.text(topics[i]);
         $("#buttons-view").append(btn);
@@ -32,4 +43,7 @@ $('#add-person').on('click', function(event){
 
     renderButtons();
 })
+
+$(document).on("click", ".sportsmen-btn", displaySportsInfo);
+
 renderButtons();
